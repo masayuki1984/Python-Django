04f56ae99c095d9899aaab8107eda3cfeef5f490
masayuki1984 <xianx.standalone@gmail.com>
2020-04-15 21:36:28 +0900

    various commit.

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,20 @@
-from django.views import generic
+import logging
 
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import InquiryForm
+
+logger = logging.getLogger(__name__)
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
